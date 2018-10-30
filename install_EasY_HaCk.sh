46b9ca4eaae0488347c4d4520c74d1c894a4f09f
sabri-zaki <37634605+sabri-zaki@users.noreply.github.com>
2018-10-30 18:54:32 +0100

    Add files via upload

---
+++ install_EasY_HaCk.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+#pkg update && pkg upgrade
+
+pkg install curl
+
+#pkg install git
+
+$git clone https://github.com/sabri-zaki/EasY_HaCk
+
+dir=$HOME/EasY_HaCk
+H=$HOME
+
+if [ -d $dir ]
+	then
+	clear
+	echo "welcom to Easyhack/"
+	sleep 5
+	chmod +x $dir/*
+	cp $dir/EasY_HaCk $H/
+	bash $dir/install.sh
+	exit
+	else
+	clear
+	echo "wait ......."
+	sleep 5
+	echo "sorry you have bad internet connection "
+	
+	echo "or you don't have space in your mobile phone"
+fi
